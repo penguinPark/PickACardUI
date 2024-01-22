a09ab865ba1ddb1423223442dbbab7c4a36bb3c5
penguinPark <park.ashl@northeastern.edu>
2024-01-22 17:34:49 +0000

    Add project files.

---
+++ PickACardUI/CardPicker.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PickACardUI
+{
+    class CardPicker
+    { 
+        static Random random = new Random();
+        public static string[] PickSomeCards(int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++)
+            {
+                pickedCards[i] = RandomValue() + "of" + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1)
+            {
+                return "Ace";
+            }
+            else if (value == 11)
+            {
+                return "Jack";
+            }
+            else if (value == 12)
+            {
+                return "Queen";
+            }
+            else if (value == 13)
+            {
+                return "King";
+            }
+            return value.ToString();
+
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1)
+            {
+                return "Spades";
+            }
+            else if (value == 2)
+            {
+                return "Hearts";
+            }
+            else if (value == 3)
+            {
+                return "Clubs";
+            }
+            return "Diamonds";
+        }
+    }
+}
