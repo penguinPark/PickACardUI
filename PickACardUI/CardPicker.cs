bc33f690daf38d90914666eea2d803f61c70bcda
penguinPark <park.ashl@northeastern.edu>
2024-01-30 22:16:47 +0000

    UPDATED// IN-CLASS UNIQUE

--- PickACardUI/CardPicker.cs
+++ PickACardUI/CardPicker.cs
@@ -1,18 +1,65 @@
 ﻿using System;
+using System.Linq;
 
-namespace PickACardUI
+namespace PickAUniqueCardUI
 {
     class CardPicker
     { 
         static Random random = new Random();
+        static string[] cards = new string[52];
+        // generates deck of card; 
+        static CardPicker()
+        {
+            string[] suits = { "Spades", "Hearts", "Clubs", "Diamonds" };
+            int cardCounter = 0;
+            for(int cardVal = 1; cardVal <=13; cardVal++)
+            {
+                foreach(string cardSuit in suits)
+                {
+                    string cardName;
+                    if(cardVal == 11)
+                    {
+                        cardName = "Jack";
+                    } else if (cardVal == 12)
+                    {
+                        cardName = "Queen";
+                    } else if (cardVal == 13)
+                    {
+                        cardName = "King";
+                    } else if (cardVal == 1)
+                    {
+                        cardName = "Ace";
+                    }
+
+                    // if number is 1 & 11-13, give it a name
+                    cardName = cardVal.ToString();
+                    cards[cardCounter] =  cardName + " of " + cardSuit;
+                    cardCounter++;
+                }
+            }
+            for (var i = 0; i <52; i++)
+            {
+                Console.WriteLine(cards[i]);
+            }
+        }
         public static string[] PickSomeCards(int numberOfCards)
         {
             string[] pickedCards = new string[numberOfCards];
             for (int i = 0; i < numberOfCards; i++)
             {
-                pickedCards[i] = RandomValue() + "of" + RandomSuit();
+                // pickedCards[i] = RandomValue() + "of" + RandomSuit();
+                pickedCards[i] = RandomCard();
             }
             return pickedCards;
+        }
+
+        private static string RandomCard()
+        {
+            int cardNum = random.Next(0, cards.Length);
+            string picked = cards[cardNum];
+            cards = cards.Where(e => e != picked).ToArray();
+            return picked;
+
         }
 
         private static string RandomValue()
--- PickACardUI/MainWindow.xaml.cs
+++ PickACardUI/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PickACardUI
+namespace PickAUniqueCardUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
